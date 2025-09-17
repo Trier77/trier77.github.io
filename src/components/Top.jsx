f5013896e68fbe345394767aaec7b435d5c51174
Trier77 <jacob.aa.trier@gmail.com>
2025-09-17 21:06:28 +0200

    ts

--- src/components/Top.jsx
+++ src/components/Top.jsx
@@ -17,17 +17,17 @@ export default function Top(){
                     <div className="sliderWrapper">
                         <div className="slider" style={{"--bill-antal": 12}}>
                         <div className="item" style={{ "--position": 1 }}><img src="img/logos/ae.png" alt="dragon1"/></div>
-                        <div className="item" style={{ "--position": 2 }}><img src="public/img/logos/pr.png" alt="dragon2"/></div>
-                        <div className="item" style={{ "--position": 3 }}><img src="public/img/logos/aff-design.png" alt="dragon3"/></div>
-                        <div className="item" style={{ "--position": 4 }}><img src="public/img/logos/figma.png" alt="dragon4"/></div>
-                        <div className="item" style={{ "--position": 5 }}><img src="public/img/logos/github.png" alt="dragon5"/></div>
-                        <div className="item" style={{ "--position": 6 }}><img src="public/img/logos/html.png" alt="dragon6"/></div>
-                        <div className="item" style={{ "--position": 7 }}><img src="public/img/logos/css.png" alt="dragon7"/></div>
-                        <div className="item" style={{ "--position": 8 }}><img src="public/img/logos/js.png" alt="dragon8"/></div>
-                        <div className="item" style={{ "--position": 9 }}><img src="public/img/logos/react.png" alt="dragon9"/></div>
-                        <div className="item" style={{ "--position": 10 }}><img src="public/img/logos/audacity.png" alt="dragon10"/></div>
-                        <div className="item" style={{ "--position": 11 }}><img src="public/img/logos/reaper.webp" alt="dragon10"/></div>
-                        <div className="item" style={{ "--position": 12 }}><img src="public/img/logos/davinci.png" alt="dragon10"/></div>
+                        <div className="item" style={{ "--position": 2 }}><img src="img/logos/pr.png" alt="dragon2"/></div>
+                        <div className="item" style={{ "--position": 3 }}><img src="img/logos/aff-design.png" alt="dragon3"/></div>
+                        <div className="item" style={{ "--position": 4 }}><img src="img/logos/figma.png" alt="dragon4"/></div>
+                        <div className="item" style={{ "--position": 5 }}><img src="img/logos/github.png" alt="dragon5"/></div>
+                        <div className="item" style={{ "--position": 6 }}><img src="img/logos/html.png" alt="dragon6"/></div>
+                        <div className="item" style={{ "--position": 7 }}><img src="img/logos/css.png" alt="dragon7"/></div>
+                        <div className="item" style={{ "--position": 8 }}><img src="img/logos/js.png" alt="dragon8"/></div>
+                        <div className="item" style={{ "--position": 9 }}><img src="img/logos/react.png" alt="dragon9"/></div>
+                        <div className="item" style={{ "--position": 10 }}><img src="img/logos/audacity.png" alt="dragon10"/></div>
+                        <div className="item" style={{ "--position": 11 }}><img src="img/logos/reaper.webp" alt="dragon10"/></div>
+                        <div className="item" style={{ "--position": 12 }}><img src="img/logos/davinci.png" alt="dragon10"/></div>
                         </div>
                     </div>            
         
